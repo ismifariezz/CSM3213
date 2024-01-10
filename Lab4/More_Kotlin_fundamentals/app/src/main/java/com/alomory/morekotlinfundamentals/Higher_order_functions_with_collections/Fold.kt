497cc84909d5d7a7bbdecdbb09b792f3e68f71d5
ismifariezz <riezz021@gmail.com>
2024-01-11 00:13:57 +0800

    deleted

--- Lab4/More_Kotlin_fundamentals/app/src/main/java/com/alomory/morekotlinfundamentals/Higher_order_functions_with_collections/Fold.kt
+++
@@ -1,8 +0,0 @@
-package com.alomory.morekotlinfundamentals.Higher_order_functions_with_collections
-
-fun main(args: Array<String>) {
-    val totalPrice = cookies.fold(0.0) {total, cookie ->
-        total + cookie.price
-    }
-    println("Total price: $${totalPrice}")
-}
